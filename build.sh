1c210c441146033fe987612dac4252ce27bba8ff
Auston Stewart <auston@shedbuilt.net>
2018-04-15 11:54:13 -1000

    Updating packaging for blank

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 mkdir build && \
 cd build && \
 cmake -DCMAKE_INSTALL_PREFIX=/usr -DLIB_SUFFIX= .. && \
-make -j $SHED_NUMJOBS && \
-make DESTDIR="$SHED_FAKEROOT" install
+make -j $SHED_NUM_JOBS && \
+make DESTDIR="$SHED_FAKE_ROOT" install
